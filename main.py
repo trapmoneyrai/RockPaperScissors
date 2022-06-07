222aa0ed8a3cae8180ebba1ad1ff1a77f82e254b
trapmoneyrai <@producedbyrai@gmail.com>
2022-06-07 23:45:02 +0200

    idk what I changed this time

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ while True:
         player = input("Rock, paper or scissors?: ").lower()
 
     if player == cpu:
-        print("Player: ", player + ". " + "CPU: ", cpu)
+        print("Player: ",player + ". " + "CPU: ",cpu + ".")
         print("Tie!")
         print("It's either win or lose. Play again.")
     elif player == "rock":
         if cpu == "paper":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("You lost.")
             break
         if cpu == "scissors":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("Winner.")
             break
     elif player == "scissors":
         if cpu == "rock":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("You lost.")
             break
         if cpu == "paper":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("Winner.")
             break
     elif player == "paper":
         if cpu == "scissors":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("You lost.")
             break
         if cpu == "rock":
-            print("Player: ", player + ". " + "CPU: ", cpu)
+            print("Player: ",player + ". " + "CPU: ",cpu + ".")
             print("Winner.")
             break
