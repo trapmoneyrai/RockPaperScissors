fd11729095d2c728d424dc409e21d7038b9742d0
trapmoneyrai <@producedbyrai@gmail.com>
2022-06-07 23:34:57 +0200

    Changed computer to CPU

--- main.py
+++ main.py
@@ -3,47 +3,47 @@ import random
 while True:
     choices = ["rock", "paper", "scissors"]
 
-    computer = random.choice(choices)
+    cpu = random.choice(choices)
     player = None
 
     while player not in choices:
         player = input("Rock, paper or scissors?: ").lower()
 
-    if player == computer:
-        print("Computer: ", computer)
+    if player == cpu:
+        print("cpu: ", cpu)
         print("Player: ", player)
         print("Tie!")
         print("It's either win or lose. Play again.")
     elif player == "rock":
-        if computer == "paper":
-            print("Computer: ", computer)
+        if cpu == "paper":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("You lost.")
             break
-        if computer == "scissors":
-            print("Computer: ", computer)
+        if cpu == "scissors":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("Winner.")
             break
     elif player == "scissors":
-        if computer == "rock":
-            print("Computer: ", computer)
+        if cpu == "rock":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("You lost.")
             break
-        if computer == "paper":
-            print("Computer: ", computer)
+        if cpu == "paper":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("Winner.")
             break
     elif player == "paper":
-        if computer == "scissors":
-            print("Computer: ", computer)
+        if cpu == "scissors":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("You lost.")
             break
-        if computer == "rock":
-            print("Computer: ", computer)
+        if cpu == "rock":
+            print("cpu: ", cpu)
             print("Player: ", player)
             print("Winner.")
             break
